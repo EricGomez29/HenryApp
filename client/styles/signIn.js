23479169813eaf034720b35012731edb1dd317bc
silverbryan <soporte.g4a@gmail.com>
2020-12-04 19:08:34 -0300

    add sigin prueba

---
+++ client/styles/signIn.js
@@ -0,0 +1,10 @@
+const { StyleSheet } = require("react-native")
+
+const Styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        marginTop: 50
+    },
+});
+
+export default Styles;
