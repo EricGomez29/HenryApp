80dbe0b73ab7f6e15163a597dc604183378a28bf
facundo-santacruz <facugs2090@gmail.com>
2020-12-04 16:23:04 -0300

    resolver.js userCreate con password hasheada

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,6 +1,5 @@
 import User from './models/Users';
-
-
+import bcrypt from 'bcrypt';
 const resolvers = {
     Query: {
         user: async (parent, { id }, context) => await User.findById(id).exec(),
@@ -8,10 +7,13 @@ const resolvers = {
     },
 
     Mutation: {
-        
-        addUser: async (parent, { input }, context) => await User.create(input),
-        editUser: async (parent, { input }, context) => await  User.findOneAndUpdate({ "username": input.username }, input ),
+        addUser: async (parent, {input }, context) => {
+            const {password} = input;
+            const hash = await bcrypt.hash(password, 9);
+            return await User.create({...input, password: hash})},
+
+            editUser: async (parent, { input }, context) => await  User.findOneAndUpdate({ "username": input.username }, input ),
         removeUser: async (parent, { username }, context) => await  User.findOneAndRemove( {"username":username})       
-}
+    }
 }
 export default resolvers;
