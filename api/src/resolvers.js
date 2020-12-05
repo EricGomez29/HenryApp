c9baf327c4ff2fac5491c9f6ea416879335e7010
EricGomez29 <eric.gomez.unq@gmail.com>
2020-12-05 17:48:04 -0300

    Modelo Cohorte agregado

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,13 +1,22 @@
 import User from './models/Users';
 import bcrypt from 'bcrypt';
-import { createToken } from './services'
+import { createToken } from './services';
+import Cohorte from './models/Cohorte';
+
+
 const resolvers = {
     Query: {
+        //USERS
         user: async (parent, { username }, context) => await User.find(username).exec(),
-        users: async (parent, { where }, context) => await User.find(where).exec()
+        users: async (parent, { where }, context) => await User.find(where).exec(),
+
+        //COHORTES
+        cohortes: async (parent, { where }, context) => await Cohorte.find(where).exec()
     },
 
     Mutation: {
+
+        //USERS
         addUser: async (parent, { input }, context) => {
             const { password } = input;
             const hash = await bcrypt.hash(password, 9);
@@ -15,7 +24,9 @@ const resolvers = {
         },
 
         editUser: async (parent, { input }, context) => await  (User.findOneAndUpdate({ "username": input.username }, input)),
-        removeUser: async (parent, { username }, context) => await  User.findOneAndRemove( {"username":username}),       
+        removeUser: async (parent, { username }, context) => await  User.findOneAndRemove({"username":username}),
+        
+        //AUTH
         login: async(_, { email, password }) => {
             const user = await User.findOne({ email: email });
             if (!user) {
@@ -27,7 +38,10 @@ const resolvers = {
                 throw new Error("Incorrect password");
             }
             return createToken(user);
-        }
+        },
+
+        //COHORTES
+        addCohorte: async (parent, { input }, context) => await Cohorte.create(input)
     }
 }
 
