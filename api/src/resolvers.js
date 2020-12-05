44395442272b9a44fd6bcccc617e7394a1716a2a
EricGomez29 <eric.gomez.unq@gmail.com>
2020-12-05 14:24:15 -0300

    Pull con rama BackEndFacu

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -2,33 +2,24 @@ import User from './models/Users';
 import bcrypt from 'bcrypt';
 const resolvers = {
     Query: {
-        user: async (parent, { id }, context) => await User.findById(id).exec(),
         users: async (parent, { where }, context) => await User.find(where).exec()
     },
 
     Mutation: {
-        addUser: async (parent, {input }, context) => {
-            const {password} = input;
+        addUser: async (parent, { input }, context) => {
+            const { password } = input;
             const hash = await bcrypt.hash(password, 9);
-            return await User.create({...input, password: hash})},
+            return await User.create({
+                ...input,
+                password: hash
+            })
+        },
 
-            editUser: async (parent, { input }, context) => {
-                const {password} = input;
-                const user = await User.findOne( {"username": input.username})
-                if (!user){
-                    throw new Error("No se encontro el usuario")
-                }else{
-                    console.log(user);
-                    const verificatedPassword = await bcrypt.compare(password, user.password);
-                    if (!verificatedPassword){
-                        throw new Error("Contraseña Incorrecta")
-                    }else{
-                         
-                        return await  User.findOneAndUpdate({ "username": input.username }, {...input, password: input.password})
-                    }
-                }
-            },
-            removeUser: async (parent, { username }, context) => await  User.findOneAndRemove( {"username":username})       
+        editUser: async (parent, { input }, context) => {
+            return await User.findOneAndUpdate({ "username": input.username }, input)
+        },
+
+        removeUser: async (parent, { username }, context) => await  User.findOneAndRemove({"username":username})       
     }
 }
 export default resolvers;
