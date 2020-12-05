e3332d8a4113a65a575e44fc3856fcb4d127bf2c
facundo-santacruz <facugs2090@gmail.com>
2020-12-05 14:22:52 -0300

    agregado login y token utilizando jwt / moment

--- api/src/index.js
+++ api/src/index.js
@@ -4,18 +4,39 @@ import resolvers from './resolvers';
 import typeDefs from './schema';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { decodeToken } from "./services";
 
 dotenv.config();
 
 const { DATABASE_URL } = process.env;
 
 mongoose.connect(DATABASE_URL, {
+    useFindAndModify: false ,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then( _ => console.log('Database is running'));
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, 
+    context: async ({ req }) => {
+    let authToken = null;
+    let user = null;
+
+    try {
+      authToken = req.headers.authorization;
+
+      if (authToken) {
+        user = await decodeToken(authToken);
+      }
+    } catch (e) {
+      console.warn(`No se pudo autenticar el token: ${authToken}`);
+    }
+    // console.log(authToken)
+    return {
+      authToken,
+      user
+    };
+  } })
 
 const app = express();
 
--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,8 +1,9 @@
 import User from './models/Users';
 import bcrypt from 'bcrypt';
+import { createToken } from './services'
 const resolvers = {
     Query: {
-        user: async (parent, { id }, context) => await User.findById(id).exec(),
+        user: async (parent, { username }, context) => await User.find(username).exec(),
         users: async (parent, { where }, context) => await User.find(where).exec()
     },
 
@@ -10,25 +11,24 @@ const resolvers = {
         addUser: async (parent, {input }, context) => {
             const {password} = input;
             const hash = await bcrypt.hash(password, 9);
-            return await User.create({...input, password: hash})},
+            return await User.create({...input, password: hash})
+        },
 
-            editUser: async (parent, { input }, context) => {
-                const {password} = input;
-                const user = await User.findOne( {"username": input.username})
-                if (!user){
-                    throw new Error("No se encontro el usuario")
-                }else{
-                    console.log(user);
-                    const verificatedPassword = await bcrypt.compare(password, user.password);
-                    if (!verificatedPassword){
-                        throw new Error("Contraseña Incorrecta")
-                    }else{
-                         
-                        return await  User.findOneAndUpdate({ "username": input.username }, {...input, password: input.password})
-                    }
-                }
-            },
-            removeUser: async (parent, { username }, context) => await  User.findOneAndRemove( {"username":username})       
+        editUser: async (parent, { input }, context) => await  (User.findOneAndUpdate({ "username": input.username }, input)),
+        removeUser: async (parent, { username }, context) => await  User.findOneAndRemove( {"username":username}),       
+        login: async(_, { email, password }) => {
+            const user = await User.findOne({ email: email });
+            if (!user) {
+                throw new Error("No hay usuario con ese email");
+            }
+            const valid = await bcrypt.compare(password, user.password);
+
+            if (!valid) {
+                throw new Error("Incorrect password");
+            }
+            return createToken(user);
+        }
     }
 }
+
 export default resolvers;
