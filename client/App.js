c1120a2554862bb8a50e5c62c95cf2b4f4a4a7d2
DAVVIDSD <david190698@gmail.com>
2020-12-05 20:30:40 -0500

    redux

--- client/App.js
+++ client/App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+// Dependencia React
 import React from 'react';
+import store from "./Redux/Store/index.js";
+import { Provider } from "react-redux";
+//  Componentes
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
-  return (
+
+    return (
+  <Provider store={store}>
     <View style={styles.container}>
       <Text>Henry APP!</Text>
       <StatusBar style="auto" />
     </View>
+  </Provider>
   );
 }
 
