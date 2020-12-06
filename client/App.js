f908bf3ad477e71b47ab1f43cf9ec59b6eeec8fe
AyeVillarruel <ayelen.villarruel.1998@gmail.com>
2020-12-06 19:35:24 -0300

    conexion fallida

--- client/App.js
+++ client/App.js
@@ -1,24 +1,29 @@
 
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import store from "./Redux/Store/index.js";
-import { Provider } from "react-redux";
-// import { StatusBar } from 'expo-status-bar';
+//import store from "./Redux/Store/index.js";
+//import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Register from './src/Components/Register'
 import Login from './screens/SignInScreen';
 import Home from './screens/HomeScreen';
+import { ApolloClient, InMemoryCache, gql ,ApolloProvider} from '@apollo/client';
 
 const Stack = createStackNavigator();
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+});
 
 export default function App() {
+  
 
   return (
-   <Provider store={store}> 
+   <ApolloProvider client={client}> 
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={Home}/>
@@ -26,18 +31,8 @@ export default function App() {
         <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>
     </NavigationContainer>
-   </Provider>
+   </ApolloProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-fontSize: [10, 20, 30]
 
