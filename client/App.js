eec1b8e9c6ef431d6d9f892f26642e877a69c87b
Maxi De Filippis <maxidefilippis@gmail.com>
2020-12-07 14:07:43 -0300

    feature (App.js) Nuevos items de navegacion de la aplicacion

--- client/App.js
+++ client/App.js
@@ -1,14 +1,15 @@
 import 'react-native-gesture-handler';
-//import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 //import store from "./Redux/Store/index.js";
 //import { Provider } from "react-redux";
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Register from './src/Components/Register'
-import Login from './screens/SignInScreen';
-import Home from './screens/HomeScreen';
+import Register from './screens/Register'
+import Login from './screens/Login';
+import Home from './screens/Home';
+import Welcome from './screens/Welcome';
+import ForgotPassword from './screens/ForgotPassword';
 import { ApolloClient, InMemoryCache, gql ,ApolloProvider} from '@apollo/client';
 
 const Stack = createStackNavigator(  );
@@ -20,7 +21,6 @@ const client = new ApolloClient({
 
 export default function App() {
   
-
   return (
    <ApolloProvider client={client}> 
     <NavigationContainer>
@@ -29,6 +29,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
         <Stack.Screen name="Welcome" component={Welcome}/>
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
       </Stack.Navigator>
     </NavigationContainer>
    </ApolloProvider>
