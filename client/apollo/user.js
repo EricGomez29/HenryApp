6509e004ebd28ebe2d1567620cf5278e4e4aa0ec
silverbryan <soporte.g4a@gmail.com>
2020-12-24 16:34:55 -0300

    actualizando el login

--- client/apollo/user.js
+++ client/apollo/user.js
@@ -48,3 +48,14 @@ export const EDIT_USER = gql`
             lastName
         }
 }`;
+
+export const LOGIN = gql`
+mutation Login($email: String!, $password: String! ) {
+    login(email: $email, password: $password) {
+        success
+        token
+        errors {
+            message
+        }
+    }
+}`;
