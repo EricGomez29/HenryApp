4a5ff0061823c9162deec02f8db66a5f01660d61
facundo-santacruz <facugs2090@gmail.com>
2021-01-07 20:30:23 -0300

    agregando perfil de usuario para el admin

--- client/screens/ProfileUser.jsx
+++ client/screens/ProfileUser.jsx
@@ -8,11 +8,11 @@ import { EDIT_USER } from '../apollo/user';
 import { useMutation } from '@apollo/client';
 import md5 from 'md5'
 
-const ProfileEdit = ({ route, navigation }) => {
+const ProfileUser = ({ route, navigation }) => {
 
     const [data, setData] = useState(route.params.modifyData);
     const [editProfile] = useMutation(EDIT_USER);
-
+    console.log(data)
     const handleSubmit = async (values) => {
         try {
             const response = await editProfile({
@@ -62,9 +62,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 size={200}
                                 source={values.image}
                             />
-                            <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate('PhotoProfile',{ data: values})}>
-                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Cambiar Imagen</Text>
-                            </TouchableOpacity>
+                            
                             <Text style={styles.textLabel}>Pais</Text>
                             <TextInput
                                 style={styles.textInput}
@@ -72,6 +70,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('nationality')}
                                 onBlur={handleBlur('nationality')}
                                 value={values.nationality}
+                                editable={false}
                             />
                             <Text style={styles.textLabel}>Nombre</Text>
                             <TextInput
@@ -80,6 +79,8 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('firstName')}
                                 onBlur={handleBlur('firstName')}
                                 value={values.firstName}
+                                editable={false}
+
                             />
                             <Text style={styles.textLabel}>Apellido</Text>
                             <TextInput
@@ -88,6 +89,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('lastName')}
                                 onBlur={handleBlur('lastName')}
                                 value={values.lastName}
+                                editable={false}
                             />
                             <Text style={styles.textLabel}>Usuario</Text>
                             <TextInput
@@ -96,6 +98,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('username')}
                                 onBlur={handleBlur('username')}
                                 value={values.username}
+                                editable={false}
                             />
                             <Text style={styles.textLabel}>Email</Text>
                             <TextInput
@@ -104,6 +107,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('email')}
                                 onBlur={handleBlur('email')}
                                 value={values.email}
+                                editable={false}
                             />
                             <Text style={styles.textLabel}>Cohorte</Text>
                             <TextInput
@@ -112,6 +116,7 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('cohorte')}
                                 onBlur={handleBlur('cohorte')}
                                 value={values.cohorte}
+                                editable={false}
                             />
                             <Text style={styles.textLabel}>Telefono</Text>
                             <TextInput
@@ -120,10 +125,21 @@ const ProfileEdit = ({ route, navigation }) => {
                                 onChangeText={handleChange('phone')}
                                 onBlur={handleBlur('phone')}
                                 value={values.phone}
+                                editable={false}
                             />
                             <View style={styles.containerBoton}>
                                 <TouchableOpacity style={styles.boton} onPress={handleSubmit}>
-                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Guardar</Text>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Cambiar de Cohorte</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.containerBoton}>
+                                <TouchableOpacity style={styles.boton} onPress={handleSubmit}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Hacerlo Administrador</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.containerBoton}>
+                                <TouchableOpacity style={styles.boton} onPress={handleSubmit}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Hacerlo PM</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -134,4 +150,4 @@ const ProfileEdit = ({ route, navigation }) => {
     )
 }
 
-export default ProfileEdit;
+export default ProfileUser;
