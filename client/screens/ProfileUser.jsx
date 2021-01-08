1eaa0a20f223a44e8fcb35ee23de1cbcd0616c37
narela <narela315@gmail.com>
2021-01-08 05:06:50 -0300

    se puede hacer Admin a otro usuario

--- client/screens/ProfileUser.jsx
+++ client/screens/ProfileUser.jsx
@@ -1,60 +1,101 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Formik } from 'formik';
 import { View, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { Avatar } from 'react-native-paper';
 import { Text } from 'react-native-paper';
 import { styles } from '../styles/ProfileEditStyles';
-import { gql, useMutation } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import Particles from './Particles';
 import MenuDesplegable from './MenuDesplegable';
+import {GET_USER} from '../apollo/user';
 
-const ProfileUser = ({ route, navigation }) => {
-    
-    const EDIT_USER = gql`mutation editUser($username: String, $isAdmin: Boolean, $isPM: Boolean) {
-	    editUser (input: {
-            username: $username
-            isAdmin: $isAdmin
-            isPM: $isPM
-        }){
-            username
-            firstName
-            lastName
-            nationality
-            phone
-            email
-            cohorte
-            image
-            isPM
-            isAdmin
-            listPM
-        }
+
+const IS_ADMIN = gql`
+mutation editUser($username: String, $isAdmin: Boolean) {
+    editUser (input: {
+        username: $username
+        isAdmin: $isAdmin
+    }){
+        username 
+        isAdmin
+        isPM
+    }
+}`;
+const IS_PM = gql`
+mutation editUser($username: String, $isAdmin: Boolean, $isPM: Boolean) {
+    editUser (input: {
+        username: $username
+        isPM: $isPM
+    }){
+        username 
+        isAdmin
+        isPM
+    }
 }`;
 
+const ProfileUser = ({ route, navigation }) => {
 
     const [data, setData] = useState(route.params.data)
-    console.log(data)
-    const [editProfile] = useMutation(EDIT_USER);
-    const handleSubmit = async (values, str) => {
-        var y;
-        if(str){
-            y = values.isAdmin === true ? false : true;
-        }else{
-            str= "isPM";
-            y = values.isPM === true ? false : true;
-        }
-        try {
-            console.log(y)
-            const response = await editProfile({
-                variables: {
-                    username: values.username,
-                    [str]: y,
-                }
-            })
-            navigation.navigate("ProfileUser", {data: response.data.editProfile})
-        } catch (error) {
-            console.log(error);    
-        }
+    const bool = JSON.parse(data.isAdmin)                    //llega como string asi que lo parseo a booleano
+    const [admin, setAdmin] = useState(bool)                 // guardo ese booleano en un estado
+    
+    const [isAdmin] = useMutation(IS_ADMIN);                //hago la mutacion solo con isAmin
+    const handleSubmit = async () => {
+        const response = await isAdmin({
+            variables: {
+                username: data.username,
+                isAdmin: !admin
+            }
+        })
+        const nuevoBool = JSON.parse(response.data.editUser.isAdmin)    //parseo de nuevo la respuesta xq llega como string
+        setAdmin(nuevoBool)                                            //lo seteo en el estado para que cambie al instante el input
     }
+    
+//     const EDIT_USER = gql`mutation editUser($username: String, $isAdmin: Boolean, $isPM: Boolean) {
+// 	    editUser (input: {
+//             username: $username
+//             isAdmin: $isAdmin
+//             isPM: $isPM
+//         }){
+//             username
+//             firstName
+//             lastName
+//             nationality
+//             phone
+//             email
+//             cohorte
+//             image
+//             isPM
+//             isAdmin
+//             listPM
+//         }
+// }`;
+
+
+//     const [data, setData] = useState(route.params.data)
+//     console.log(data)
+//     const [editProfile] = useMutation(EDIT_USER);
+//     const handleSubmit = async (values, str) => {
+//         var y;
+//         if(str){
+//             y = values.isAdmin === true ? false : true;
+//         }else{
+//             str= "isPM";
+//             y = values.isPM === true ? false : true;
+//         }
+//         try {
+//             console.log(y)
+//             const response = await editProfile({
+//                 variables: {
+//                     username: values.username,
+//                     [str]: y,
+//                 }
+//             })
+//             navigation.navigate("ProfileUser", {data: response.data.editProfile})
+//         } catch (error) {
+//             console.log(error);    
+//         }
+//     }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -67,15 +108,15 @@ const ProfileUser = ({ route, navigation }) => {
             <View style={styles.userInfoSection}>
                 <Formik
                     initialValues={{
-                        country: data.nationality || '',
+                        country: data.nationality,
                         firstName: data.firstName,
                         lastName: data.lastName,
                         username: data.username,
                         email: data.email,
-                        nationality: data.nationality || '',
-                        phone: data.phone || '',
-                        cohorte: data.cohorte || '',
-                        nroTelefono: data.phone || '',
+                        nationality: data.nationality ,
+                        phone: data.phone ,
+                        cohorte: data.cohorte ,
+                        nroTelefono: data.phone ,
                         image: data.image || `https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50s=200`,
                         isAdmin: data.isAdmin,
                         isPM: data.isPM,
@@ -170,7 +211,7 @@ const ProfileUser = ({ route, navigation }) => {
                                     placeholder="Cohorte"
                                     onChangeText={handleChange('isAdmin')}
                                     onBlur={handleBlur('isAdmin')}
-                                    value={values.isAdmin}
+                                    value={admin}
                                     editable={false}
                                 />
 
@@ -187,13 +228,13 @@ const ProfileUser = ({ route, navigation }) => {
                                 
                             
                             <View style={styles.containerBoton}>
-                                <TouchableOpacity style={styles.boton} onPress={() => handleSubmit(values, "admin")}>
-                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{!values.isAdmin ? "Hacerlo Administrador" : "Deshacer Administrador"}</Text>
+                                <TouchableOpacity style={styles.boton} onPress={handleSubmit}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{!admin ? "Hacerlo Administrador" : "Deshacer Administrador"}</Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={styles.containerBoton}>
-                                <TouchableOpacity style={styles.boton} onPress={handleSubmit}>
-                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{!values.isPM ? "Hacerlo PM" : "Deshacer PM"}</Text>
+                                <TouchableOpacity style={styles.boton} onPress={handleSubmit2}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>{!data.isPM ? "Hacerlo PM" : "Deshacer PM"}</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
